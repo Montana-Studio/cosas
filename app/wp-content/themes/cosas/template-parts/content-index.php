ea905754362b0e55990567fa9176290d2b2e5723
argkont <kohacaelo@gmail.com>
2016-04-25 18:01:47 -0300

    update 24/04 Front

--- app/wp-content/themes/cosas/template-parts/content-index.php
+++ app/wp-content/themes/cosas/template-parts/content-index.php
@@ -9,22 +9,36 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url('');">
-	<?php get_permalink( get_post_thumbnail_id( $post->ID ) ); ?>
-	<a href="<?php echo get_permalink(); ?>"><i class="fa fa-plus-circle"></i></a>
-	<?php
+<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 720,405 ), false, '' ); ?>
 
-        if ( has_post_thumbnail() ) {
-            the_post_thumbnail();
-        }
-	?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url('<?php echo $src[0]; ?>');">
+	<a href="<?php echo get_permalink(); ?>" class="more-link"><i class="fa fa-plus-circle"></i></a>
+
 	<footer class="entry-footer">
 		<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}the_time('l, F g a');
+
+			$cats = get_the_category();
 		?>
+
+			<span><?php echo $cats[0]->cat_name;?></span>
+		
+		<h2 class="entry-title"><a href="<?php echo esc_url( get_permalink() );?>" rel="bookmark">
+		<?php 
+
+			if (strlen($post->post_title) > 35) {
+				
+				echo substr(the_title($before = '', $after = '', FALSE), 0, 35) . '...';
+
+			} else {
+				the_title();
+			} 
+		}?>
+		</a></h2>
+			<div class="times-post">
+				<i class="fa fa-clock-o"></i> <?php the_time('l, F g A');?>
+			</div>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
