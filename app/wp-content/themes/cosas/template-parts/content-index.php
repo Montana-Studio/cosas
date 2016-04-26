5ac53b7a9c6078c58a7976c47e27191b78242ea1
argkont <kohacaelo@gmail.com>
2016-04-26 10:02:10 -0300

    update 25/04 MY MAIN.SCSS

--- app/wp-content/themes/cosas/template-parts/content-index.php
+++ app/wp-content/themes/cosas/template-parts/content-index.php
@@ -17,10 +17,15 @@
 		<div class="img-post" style="background-image: url('<?php echo $src[0]; ?>');">
 			<a href="<?php echo get_permalink(); ?>" class="more-link"><i class="fa fa-plus-circle"></i></a>
 		</div>
-	<?php }else{ ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url('<?php echo $src[0]; ?>');">
-
+	<?php }elseif(is_category(array('personaje-nacional','politica-nacional','espectaculos-nacional'))){ ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="img-post" style="background-image: url('<?php echo $src[0]; ?>');">
 			<a href="<?php echo get_permalink(); ?>" class="more-link"><i class="fa fa-plus-circle"></i></a>
+		</div>
+	<?php }else{ ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url('<?php echo $src[0]; ?>');">
+
+		<a href="<?php echo get_permalink(); ?>" class="more-link"><i class="fa fa-plus-circle"></i></a>
 	<?php }
 ?>
 
