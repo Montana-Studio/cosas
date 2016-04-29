044c6704ba8e60a0bde48a535d3aad838c933a3b
marlenetiare <marlene.jamett@gmail.com>
2016-04-29 11:43:32 -0400

    update footer

--- app/wp-content/themes/casas/sidebar.php
+++ app/wp-content/themes/casas/sidebar.php
@@ -10,16 +10,7 @@
 
 	<div class="facebook-widget">
 
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.6";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-
-	<div class="fb-page" data-href="https://www.facebook.com/RevistaCASAS/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/RevistaCASAS/"><a href="https://www.facebook.com/RevistaCASAS/">Revista CASAS</a></blockquote></div></div>
+		<div class="fb-page" data-href="https://www.facebook.com/RevistaCASAS/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/RevistaCASAS/"><a href="https://www.facebook.com/RevistaCASAS/">Revista CASAS</a></blockquote></div></div>
 
 	</div>
 	
