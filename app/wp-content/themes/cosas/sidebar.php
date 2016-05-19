d86cbf1388ccd593c182d3f48843f7caeccd2862
argkont <kohacaelo@gmail.com>
2016-05-19 13:04:47 -0400

    sidebar with ad baldosas

--- app/wp-content/themes/cosas/sidebar.php
+++ app/wp-content/themes/cosas/sidebar.php
@@ -1,6 +1,13 @@
 <!-- sidebar -->
 <aside class="sidebar hide">
    
+    <div style='width:300px;margin:0 auto 5px;'>
+
+        <ins data-revive-zoneid="12" data-revive-target="_blank" data-revive-ct0="{clickurl_enc}" data-revive-id="57a6b4ea911480853c3dc7a69f930632"></ins>
+        <script async src="//bloques.cosas.com/activos/www/delivery/asyncjs.php"></script>
+   
+    </div>
+    
     <div style='width:250px;margin:0 auto 5px;'>
 
         <ins data-revive-zoneid="11" data-revive-target="_blank" data-revive-ct0="{clickurl_enc}" data-revive-id="57a6b4ea911480853c3dc7a69f930632"></ins>
