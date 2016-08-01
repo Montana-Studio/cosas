a10ae7cc8d002cb5ab2e69eef2cb049a23619e1d
argkont <kohacaelo@gmail.com>
2016-08-01 10:33:12 -0400

    search view

--- app/wp-content/themes/cosas/search.php
+++ app/wp-content/themes/cosas/search.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
     <h1>
-        <?php echo sprintf( __( '%s Resultados para &nbsp;<span>', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></span>
+		<span><?php echo sprintf( __( '%s Resultados para &nbsp;<span>', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></span>
     </h1>
 <section>
 
